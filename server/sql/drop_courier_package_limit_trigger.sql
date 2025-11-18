ede65e4d5c18c433d9adea17fc4841ab64bf41cc
EdwardVNguyen <edwardnguyen568@gmail.com>
2025-11-18 16:10:26 -0600

    removed sql migrations

--- server/sql/drop_courier_package_limit_trigger.sql
+++
@@ -1,5 +0,0 @@
--- Drop courier package limit triggers
--- The 5 package limit is now enforced at the application level instead of database level
-
-DROP TRIGGER IF EXISTS before_package_insert_check_courier_limit;
-DROP TRIGGER IF EXISTS before_package_update_check_courier_limit;
