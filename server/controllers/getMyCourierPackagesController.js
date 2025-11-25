acee779e6dd3e94f4714e9a4f50e1277b026421a
Spycrab06 <71094769+Spycrab06@users.noreply.github.com>
2025-11-24 23:07:47 -0600

    courier inventory fix 2

--- server/controllers/getMyCourierPackagesController.js
+++ server/controllers/getMyCourierPackagesController.js
@@ -25,7 +25,7 @@ export const getMyCourierPackagesController = async (req, res) => {
 
     const courierId = courierRows[0].employee_id;
 
-    // Get packages assigned to this courier that are in transit or out for delivery
+    // Get packages assigned to this courier that are in transit
     const query = `
       SELECT
         p.package_id,
@@ -42,7 +42,7 @@ export const getMyCourierPackagesController = async (req, res) => {
       LEFT JOIN customer recipient ON p.recipient_id = recipient.customer_id
       LEFT JOIN address recipient_addr ON recipient.address_id = recipient_addr.address_id
       WHERE p.courier_id = ?
-      AND p.package_status IN ('in-transit', 'out-for-delivery')
+      AND p.package_status = 'in-transit'
       ORDER BY p.created_at DESC
     `;
 
